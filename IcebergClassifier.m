294a9ac855567fbb2077e9297724655eb38b54aa
Stephen Allan <stephenwa3@yahoo.com>
2018-04-03 19:13:09 -0400

    Restructured visualization. Added usage of visualization.

--- IcebergClassifier.m
+++ IcebergClassifier.m
@@ -5,8 +5,24 @@
 % @author Douglas Lee <dxl7697>
 
 
-function IcebergClassifier()
+function result = IcebergClassifier(jsonFilepath)
     % ICEBERGCLASSIFIER TODO: Summary
     %   TODO: Description
     
+    %% Parse Arguments
+    if ~ischar(jsonFilepath)
+        error('IcebergClassifier First parameter must be a character vector <''filepath''>');
+    end
+    
+    fileID = fopen(jsonFilepath, 'r');
+    rawData = fread(fileID, '*char');
+    fclose(fileID);
+    
+    data = jsondecode(rawData);
+    
+    %% Visualize Data
+    visualizeData(data(1));
+    
+    
+    result = data;
 end
--- visualizeData.m
+++ visualizeData.m
@@ -5,6 +5,19 @@
 % @author Douglas Lee <dxl7697>
 
 
+function visualizeData(data)
+    % VISUALIZEBAND TODO: Summary
+    %   TODO: Description
+    
+    band1Image = bandToImage(data.band_1);
+    band2Image = bandToImage(data.band_2);
+    
+    figure; imshow(band1Image, []);
+    figure; imshow(band2Image, []);
+    
+    fprintf('%s: Angle = %f, Iceberg = %d\n', data.id, data.inc_angle, data.is_iceberg);
+end
+
 function bandImage = bandToImage(bandData)
     % BANDTOIMAGE TODO: Summary
     %   TODO: Description
