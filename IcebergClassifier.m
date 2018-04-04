e9b000671649cb43a3b7640390421693227b945f
Stephen Allan <stephenwa3@yahoo.com>
2018-04-03 21:53:43 -0400

    Added object detection.

--- IcebergClassifier.m
+++ IcebergClassifier.m
@@ -20,9 +20,62 @@ function result = IcebergClassifier(jsonFilepath)
     
     data = jsondecode(rawData);
     
-    %% Visualize Data
-    visualizeData(data(1));
+    testData = data(6);
     
+    %% Visualize Data
+    visualizeData(testData);
+    
+    %% Crop Object out of Image
+    subImages = detectObject(testData);
+    figure; imshowpair(subImages{1}, subImages{2}, 'montage');
+
     
     result = data;
+end
+
+function visualizeData(data)
+    % VISUALIZEDATA TODO: Summary
+    %   TODO: Description
+    
+    band1Image = bandToImage(data.band_1);
+    band2Image = bandToImage(data.band_2);
+    
+    figure; imshowpair(band1Image, band2Image, 'montage');
+    
+    fprintf('%s: Angle = %f, Iceberg = %d\n', data.id, data.inc_angle, data.is_iceberg);
+end
+
+function subImages = detectObject(data)
+    % DETECTOBJECT TODO: Summary
+    %   TODO: Description
+    
+    % FIXME: Some images are too bright and this threshold doesn't work.
+    brightnessThreshold = 140;
+    
+    bands = {data.band_1, data.band_2};
+    subImages = {length(bands)};
+    
+    for i = 1:length(bands)
+        image = bandToImage(bands{i});
+        binaryImage = image > brightnessThreshold;
+
+        figure; imshow(binaryImage);
+
+        imageStats = regionprops(binaryImage, 'BoundingBox', 'Area');
+        for j = 1:length(imageStats)
+            % Check that the region is big enough to be an object
+            if imageStats(j).Area > 15
+                box = imageStats(j).BoundingBox;  % box = [y, x, width, height]
+                rectangle('Position', [box(1), box(2), box(3), box(4)], 'EdgeColor', [1, 0, 0], 'LineWidth', 1);
+                
+                % Get a sub-image of just the individual object
+                cols = box(1) + 1:box(1) + box(3);
+                rows = box(2) + 1:box(2) + box(4);
+                subImages{i} = binaryImage(round(rows) - 1, round(cols) - 1);
+                
+                % One object per image
+                break;
+            end
+        end
+    end
 end
